2a4cebc22be40a5424257ee78dfeca64bd70d2d3
Akila Welihinda <akilawelihinda@ucla.edu>
2018-02-17 22:02:36 -0800

    added checkExistence test and improved state accumulation test

--- test/CasinoTest.js
+++ test/CasinoTest.js
@@ -3,13 +3,20 @@ var Casino = artifacts.require("Casino");
 
 contract('Casino', function(accounts) {
   it("should say new player doesn't exist", async function() {
-    const casino = await Casino.deployed();
+    const casino = await Casino.new();
     const existence = await casino.checkPlayerExists.call(accounts[0], {from: accounts[0]});
     assert(existence === false);
   });
 
+  it("should say registered player does exist", async function() {
+    const casino = await Casino.new();
+    await casino.bet(5, {from: accounts[0], value: 1});
+    const existence = await casino.checkPlayerExists.call(accounts[0], {from: accounts[0]});
+    assert(existence === true);
+  });
+
   it("should accept a few bets and accumulate state properly", async function() {
-    const casino = await Casino.deployed();
+    const casino = await Casino.new();
     const betAmt = 1;
     const betNumber = 4;
     for(var i=0; i<accounts.length; i++) {
@@ -17,5 +24,7 @@ contract('Casino', function(accounts) {
     }
     const totalBet = await casino.totalBet.call();
     assert.equal(totalBet.toNumber(), betAmt * accounts.length);
+    const totalBetCount = await casino.numberOfBets.call();
+    assert.equal(totalBetCount.toNumber(), accounts.length);
   });
 });
